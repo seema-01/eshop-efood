8c538ab577b70d98794e50be72401adc19175e94
jay <infinitietechnologies05@gmail.com>
2021-10-23 17:32:16 +0530

    initail commit

---
+++ Assets/js/main.js
@@ -0,0 +1,91 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 50,
+    slidesPerGroup: 1,
+
+    loop: true,
+    loopFillGroupWithBlank: true,
+
+
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".next",
+        prevEl: ".prev",
+    },
+
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        '@200': {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        500: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        640: {
+            slidesPerView: 3,
+            spaceBetween: 10,
+        }
+    },
+});
+
+
+
+
+var swiper = new Swiper(".popular-item", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    breakpoints: {
+        200: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        300: {
+            slidesPerView: 1,
+            spaceBetween: 30,
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 30,
+        },
+        768: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+        },
+        1200: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+
+
+
+    }
+});
